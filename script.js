5d719eccce6523d7b7205b6dd7b1c55ae150a757
palsakshi <pal74089@gmail.com>
2024-09-07 12:41:49 +0530

    add favicon

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right= "-200px";
 }
+
+// google form//
+
+const message = document.getElementById("msg");
+const form = document.forms["submit-this-google-form"];
+const scriptURL = 'https://script.google.com/d/10hTCoHjN3weP2GZ3IZ7XgkSLl1ih-bneu7XAggEjeft2fbkoKbrvkwaX/edit?usp=sharing';
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message Sent Successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 4000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
